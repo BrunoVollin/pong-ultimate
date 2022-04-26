8fd4306a474a072b52dcb807b3ed22c7f2c7f3aa
bio353 <fabiomcn2001@gmail.com>
2022-04-25 23:25:44 -0300

    Comments removed on thread

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from source.utils.cli import cls
 if __name__ == "__main__":
     cls()
     game = Game()
-    # thread = Thread(target=game.increase_ball_speed)
-    # thread.start()
+    thread = Thread(target=game.increase_ball_speed)
+    thread.start()
     game.run()
