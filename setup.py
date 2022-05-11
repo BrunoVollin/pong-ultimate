45e22e7ca8ce7990ec6bd95c6637e71f2d59f8b9
bio353 <fabiomcn2001@gmail.com>
2022-05-11 20:20:22 -0300

    Added a setup file for cx_Freeze

---
+++ setup.py
@@ -0,0 +1,23 @@
+import sys
+from cx_Freeze import setup, Executable
+
+include_files = ["assets/", "README.md", "LICENSE"]
+
+# Dependencies are automatically detected, but it might need fine tuning.
+# "packages": ["os"] is used as example only
+build_exe_options = {"packages": ["os"], "excludes": ["tkinter"],
+                     "include_files": include_files}
+
+# base="Win32GUI" should be used only for Windows GUI app
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(
+    name="pong-ultimate",
+    version="1.1",
+    description="He will rise...",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("main.py", base=base)],
+    icon="assets/images/app-icon.ico"
+)
