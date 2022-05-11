344d24a6807b5dac4ec760eeba6e0c41607e8de3
bio353 <fabiomcn2001@gmail.com>
2022-05-11 20:01:30 -0300

    Updated entity

--- source/model/entity.py
+++ source/model/entity.py
@@ -2,12 +2,12 @@ import pygame as pg
 
 
 class Entity(pg.sprite.Sprite):
-    def __init__(self, categories, pos, size, base_speed, color):
-        super().__init__(categories)
+    def __init__(self, groups, pos, size, base_speed, color):
+        super().__init__(groups)
         self.image = pg.Surface(size)
         self.image.fill(color)
         self.rect = self.image.get_rect(center=pos)
-        self.hitbox = self.rect
-        self.direction = pg.math.Vector2(0, 0)
-        self.base_speed = base_speed
-        self.speed = self.base_speed
+        self._hitbox = self.rect
+        self._direction = pg.math.Vector2(0, 0)
+        self._base_speed = base_speed
+        self._speed = self._base_speed
